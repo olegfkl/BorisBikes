0eab50ab8fd885e3e4ac94f9647294924a3c006c
Tabish Raza <tabishraza@gmail.com>
2017-10-27 09:40:08 +0100

    extracted available bike count full? & empty? to private

--- lib/Docking_station.rb
+++ lib/Docking_station.rb
@@ -7,21 +7,24 @@ class DockingStation
   end
 
   def release_bike
-    raise "There are no bikes docked" unless @available.length>0
+    raise "There are no bikes docked" unless empty?
     @available.pop
   end
 
   def dock(bike)
     @bikes=bike
-    p "checking if bike", bike
-    p "checking array available length", @available.length
-    raise "Dock is full" if @available.length >= 20
-
-    p "AFTER RAISE:checking array available length", @available.length
+    raise "Dock is full" if full?
     @available << bike
-    p "PRINT ARRAY:", @available
-    p "LENGTH ARRAY", @available.length
-   #we use an instance variable to store the bike we dock
+  end
+
+  private
+
+  def full?
+    @available.length >= 20? true:false
+  end
+
+  def empty?
+    @available.length > 0? true:false
   end
 
 end
